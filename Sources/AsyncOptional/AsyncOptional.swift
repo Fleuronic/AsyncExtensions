ef2662a8c93f9bea38af97cddbfadafe025e2797
Jordan Kay <jordan@fleuronics-mbp.myfiosgateway.com>
2021-12-09 19:06:09 -0500

    Add remaining platforms

--- Sources/AsyncOptional/AsyncOptional.swift
+++ Sources/AsyncOptional/AsyncOptional.swift
@@ -1,6 +1,6 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-@available(iOS 15, *)
+@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
 public extension Optional {
 	func asyncMap<U>(_ transform: (Wrapped) async throws -> U) async rethrows -> U? {
 		guard let wrapped = self else { return nil }
