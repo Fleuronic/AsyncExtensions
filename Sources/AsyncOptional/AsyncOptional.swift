35f80dc1b454f2fadef275b997c6af3b862ca0d3
Jordan Kay <jordan@fleuronics-mbp.myfiosgateway.com>
2021-12-10 17:41:59 -0500

    Only include availability check if necessary

--- Sources/AsyncOptional/AsyncOptional.swift
+++ Sources/AsyncOptional/AsyncOptional.swift
@@ -1,7 +1,9 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 #if swift(>=5.5)
+#if swift(<5.5.2)
 @available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
+#endif
 public extension Optional {
 	func asyncMap<U>(_ transform: (Wrapped) async throws -> U) async rethrows -> U? {
 		guard let wrapped = self else { return nil }
