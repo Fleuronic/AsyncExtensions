5b8b0ed58c3d55aba97b3e779d602a0fc32ac153
Jordan Kay <jordan@fleuronic.com>
2021-12-21 11:53:28 -0500

    Support first values and results

---
+++ Sources/AsyncExtensions/Publisher.swift
@@ -0,0 +1,85 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import Combine
+
+#if swift(<5.5.2)
+@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
+public extension Publisher {
+	var firstOutput: Output {
+		get async throws {
+			try await firstResult.get()
+		}
+	}
+
+	var firstResult: Result<Output, Failure> {
+		get async {
+			var cancellable: AnyCancellable?
+
+			return await withCheckedContinuation { continuation in
+				cancellable = sink { completion in
+					guard case let .failure(error) = completion else { return }
+					continuation.resume(returning: .failure(error))
+				} receiveValue: { value in
+					cancellable?.cancel()
+					continuation.resume(returning: .success(value))
+				}
+			}
+		}
+	}
+}
+
+@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
+public extension Publisher where Failure == Never {
+	var firstOutput: Output {
+		get async {
+			var cancellable: AnyCancellable?
+
+			return await withCheckedContinuation { continuation in
+				cancellable = sink { value in
+					cancellable?.cancel()
+					continuation.resume(returning: value)
+				}
+			}
+		}
+	}
+}
+#else
+public extension Publisher {
+	var firstOutput: Output {
+		get async throws {
+			try await firstResult.get()
+		}
+	}
+
+	var firstResult: Result<Output, Failure> {
+		get async {
+			var cancellable: AnyCancellable?
+
+			return await withCheckedContinuation { continuation in
+				cancellable = sink { completion in
+					guard case let .failure(error) = completion else { return }
+					continuation.resume(returning: .failure(error))
+				} receiveValue: { value in
+					cancellable?.cancel()
+					continuation.resume(returning: .success(value))
+				}
+			}
+		}
+	}
+}
+
+public extension Publisher where Failure == Never {
+	var firstOutput: Output {
+		get async {
+			var cancellable: AnyCancellable?
+
+			return await withCheckedContinuation { continuation in
+				cancellable = sink { value in
+					cancellable?.cancel()
+					continuation.resume(returning: value)
+				}
+			}
+		}
+	}
+}
+#endif
