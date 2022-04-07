356bb5c561564463f6883531b55bbeffe216ae6f
Jordan Kay <jordan@fleuronic.com>
2022-04-07 19:21:27 -0400

    Simplify dependencies

--- Package.swift
+++ Package.swift
@@ -1,61 +1,38 @@
-// swift-tools-version:5.5
-// Copyright © Fleuronic LLC. All rights reserved.
+// swift-tools-version:5.3
 
 import PackageDescription
 
+var dependencies: [Package.Dependency] = []
+var targetDependencies: [Target.Dependency] = []
+
 #if os(Linux)
-let package = Package(
-	name: "AsyncExtensions",
-	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_15),
-		.watchOS(.v6),
-		.tvOS(.v13)
-	],
-	products: [
-		.library(
-			name: "AsyncExtensions",
-			targets: ["AsyncExtensions"]
-		)
-	],
-	dependencies: [
-		.package(url: "https://github.com/cx-org/CombineX", from: "0.4.0")
-	],
-	targets: [
-		.target(
-			name: "AsyncExtensions",
-			dependencies: ["CombineX"]
-		),
-		.testTarget(
-			name: "AsyncExtensionsTests",
-			dependencies: ["AsyncExtensions"]
-		)
-	]
-)
-#else
-let package = Package(
-	name: "AsyncExtensions",
-	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_15),
-		.watchOS(.v6),
-		.tvOS(.v13)
-	],
-	products: [
-		.library(
-			name: "AsyncExtensions",
-			targets: ["AsyncExtensions"]
-		)
-	],
-	targets: [
-		.target(
-			name: "AsyncExtensions",
-			dependencies: []
-		),
-		.testTarget(
-			name: "AsyncExtensionsTests",
-			dependencies: ["AsyncExtensions"]
-		)
-	]
-)
+dependencies.append(.package(url: "https://github.com/cx-org/CombineX", from: "0.4.0"))
+targetDependencies.append("CombineX")
 #endif
+
+let package = Package(
+	name: "AsyncExtensions",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.watchOS(.v6),
+		.tvOS(.v13)
+	],
+	products: [
+		.library(
+			name: "AsyncExtensions",
+			targets: ["AsyncExtensions"]
+		)
+	],
+	dependencies: dependencies,
+	targets: [
+		.target(
+			name: "AsyncExtensions",
+			dependencies: targetDependencies
+		),
+		.testTarget(
+			name: "AsyncExtensionsTests",
+			dependencies: ["AsyncExtensions"]
+		)
+	]
+)
--- Sources/AsyncExtensions/Publisher.swift
+++ Sources/AsyncExtensions/Publisher.swift
@@ -2,85 +2,10 @@
 
 #if canImport(Combine)
 import Combine
-#else
+#elseif canImport(CombineX)
 import CombineX
 #endif
 
-#if swift(<5.5.2)
-@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
-public extension Publisher {
-	var singleValue: Output {
-		get async throws {
-			try await singleResult.get()
-		}
-	}
-
-	var singleResult: Result<Output, Failure> {
-		get async {
-			var cancellable: AnyCancellable?
-
-			return await withCheckedContinuation { continuation in
-				cancellable = sink { completion in
-					guard case let .failure(error) = completion else { return }
-					continuation.resume(returning: .failure(error))
-				} receiveValue: { value in
-					cancellable?.cancel()
-					continuation.resume(returning: .success(value))
-				}
-			}
-		}
-	}
-}
-
-@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
-public extension Publisher where Output == Never {
-	var completion: Subscribers.Completion<Failure> {
-		get async {
-			var cancellable: AnyCancellable?
-
-			return await withCheckedContinuation { continuation in
-				cancellable = sink { completion in
-					cancellable?.cancel()
-					continuation.resume(returning: completion)
-				} receiveValue: { _ in }
-			}
-		}
-	}
-}
-
-@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
-public extension Publisher where Failure == Never {
-	var singleValue: Output {
-		get async {
-			var cancellable: AnyCancellable?
-
-			return await withCheckedContinuation { continuation in
-				cancellable = sink { value in
-					cancellable?.cancel()
-					continuation.resume(returning: value)
-				}
-			}
-		}
-	}
-}
-
-@available(iOS 15, macOS 12, watchOS 8, tvOS 15, *)
-public extension Publisher where Output == Never, Failure == Never {
-	var completion: Void {
-		get async {
-			var cancellable: AnyCancellable?
-
-			return await withCheckedContinuation { continuation in
-				cancellable = sink { completion in
-					cancellable?.cancel()
-					continuation.resume(returning: ())
-				} receiveValue: { _ in }
-			}
-		}
-	}
-}
-
-#else
 public extension Publisher {
 	var singleValue: Output {
 		get async throws {
@@ -149,4 +74,3 @@ public extension Publisher where Output == Never, Failure == Never {
 		}
 	}
 }
-#endif
